a9ecd3a65524785c95292d1bddf9d7cdbbbc5250
Victor Weeks <vicweeks@cs.colostate.edu>
2018-04-10 15:03:34 -0600

    added some spark examples

---
+++ cs455/spark/sample/src/main/java/SimpleApp.java
@@ -0,0 +1,18 @@
+/* SimpleApp.java */
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.Dataset;
+
+public class SimpleApp {
+  public static void main(String[] args) {
+    String logFile = "/usr/local/spark/README.md"; // Should be some file on your system
+    SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+    Dataset<String> logData = spark.read().textFile(logFile).cache();
+
+    long numAs = logData.filter(s -> s.contains("a")).count();
+    long numBs = logData.filter(s -> s.contains("b")).count();
+
+    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+
+    spark.stop();
+  }
+}
