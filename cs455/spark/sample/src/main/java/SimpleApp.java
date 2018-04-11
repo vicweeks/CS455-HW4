665dc4bd6ce81094cab5ecb84988b32d4f1471a1
Victor Weeks <vicweeks@cs.colostate.edu>
2018-04-11 11:53:33 -0600

    spark sample works

--- cs455/spark/sample/src/main/java/SimpleApp.java
+++ cs455/spark/sample/src/main/java/SimpleApp.java
@@ -4,7 +4,7 @@ import org.apache.spark.sql.Dataset;
 
 public class SimpleApp {
   public static void main(String[] args) {
-    String logFile = "/usr/local/spark/README.md"; // Should be some file on your system
+    String logFile = "/data/preprocessed/WeatherData-r-00000"; // Should be some file on your system
     SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
     Dataset<String> logData = spark.read().textFile(logFile).cache();
 
