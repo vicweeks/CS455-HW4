3fbd425c854e3de2d8a1211bb76ca32fd83bca0e
Josiah May <josiahm@rams.colostate.edu>
2018-04-23 17:25:58 -0600

    add mean of array

--- cs455/spark/hw4/src/main/java/Song.java
+++ cs455/spark/hw4/src/main/java/Song.java
@@ -118,5 +118,15 @@ public class Song implements Serializable {
         this.segments_pitches = segments_pitches;
     }
 
+    private double getMeanOfArray(double[] data){
+        double total = 0.0;
+        int size = data.length -1;
+        double diff = 0.0;
+        for (int i = 0; i < size; i++) {
+            diff = data[i+1] - data[i];
+            total += diff;
+        }
+        return total;
+    }
 
 }
